287c8a727d552bfbeca7019e300a7cb44efffd2b
Node <8974108+qwenode@users.noreply.github.com>
2021-09-01 17:44:58 +0800

    异常助手

---
+++ src/ExceptionHelper.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace qwephp;
+
+use ErrorException;
+
+class ExceptionHelper
+{
+    /**
+     * @param $any
+     * @param $message
+     * @throws ErrorException
+     */
+    public static function throwIfNull($any, $message)
+    {
+        if ($any == null) {
+            throw new ErrorException($message);
+        }
+    }
+
+    /**
+     * @param $any
+     * @param $message
+     * @throws ErrorException
+     */
+    public static function throwIfZero($any, $message)
+    {
+        if ((int)$any == 0) {
+            throw new ErrorException($message);
+        }
+    }
+
+    /**
+     * @param string $any
+     * @param $message
+     * @throws ErrorException
+     */
+    public static function throwIfLengthZero(string $any, $message)
+    {
+        if (strlen(trim($any)) == 0) {
+            throw new ErrorException($message);
+        }
+    }
+}
