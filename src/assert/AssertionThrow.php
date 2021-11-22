a63befe9d5b6a889f02a832a760f8e689857fada
Node <8974108+qwenode@users.noreply.github.com>
2021-11-22 16:24:09 +0800

    add assertion

--- src/assert/AssertionThrow.php
+++ src/assert/AssertionThrow.php
@@ -20,4 +20,30 @@ class AssertionThrow
             throw new ErrorException($message);
         }
     }
+
+    /**
+     * @param string $value
+     * @param int $min
+     * @param string $message
+     * @throws ErrorException
+     */
+    public static function minLength(string $value, int $min, string $message = '数据长度太短')
+    {
+        if (!Assertion::minLength($value, $min)) {
+            throw new ErrorException($message);
+        }
+    }
+
+    /**
+     * @param string $value
+     * @param int $max
+     * @param string $message
+     * @throws ErrorException
+     */
+    public static function maxLength(string $value, int $max, string $message = '数据长度太长')
+    {
+        if (!Assertion::maxLength($value, $max)) {
+            throw new ErrorException($message);
+        }
+    }
 }
