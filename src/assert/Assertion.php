15f9ad0a1b695a93910af6111a67b5c351bcfed1
Node <8974108+qwenode@users.noreply.github.com>
2021-11-22 16:22:04 +0800

    add assertion

--- src/assert/Assertion.php
+++ src/assert/Assertion.php
@@ -1,6 +1,11 @@
 <?php
 
 namespace qwephp\assert;
+use function function_exists;
+use function mb_detect_encoding;
+use function mb_strlen;
+use function strlen;
+
 /**
  *
  */
@@ -29,4 +34,38 @@ class Assertion
         return true;
     }
 
+    /**
+     * 确保字符串包含一定数量的字符
+     * @param string $value
+     * @param int $min
+     * @return bool
+     */
+    public static function minLength(string $value, int $min)
+    {
+        return self::strlen($value) >= $min;
+    }
+
+    /**
+     * 确保字符串不超过限定长度
+     * @param string $value
+     * @param int $max
+     * @return bool
+     */
+    public static function maxLength(string $value, int $max)
+    {
+        return self::strlen($value) <= $max;
+    }
+
+    protected static function strlen($value)
+    {
+        if (!function_exists('mb_detect_encoding')) {
+            return strlen($value);
+        }
+
+        if (false === $encoding = mb_detect_encoding($value)) {
+            return strlen($value);
+        }
+
+        return mb_strlen($value, $encoding);
+    }
 }
