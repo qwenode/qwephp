1853e9c6d128bf4a63f577ecc5afe594aedd0fb5
Node <8974108+qwenode@users.noreply.github.com>
2022-08-26 15:38:40 +0800

    add ConsoleLog.php

--- src/ConsoleLog.php
+++ src/ConsoleLog.php
@@ -8,51 +8,50 @@
 namespace qwephp;
 
 /**
- * @see ConsoleLog
- * @deprecated
+ * print msg to console
  */
-class ConsoleOutput
+class ConsoleLog
 {
     //是否关闭日志打印
-    public static $daemonize = FALSE;
-
+    public static bool $disabledOutput = FALSE;
+    
     public static function error($message, ...$params)
     {
-        static::log('ERROR', $message, $params);
+        static::_print('ERROR', $message, $params);
         return 1;
     }
-
+    
     public static function info($message, ...$params)
     {
-        static::log('INFO', $message, $params);
+        static::_print('INFO', $message, $params);
         return 0;
     }
-
+    
     public static function warning($message, ...$params)
     {
-        static::log('WARNING', $message, $params);
+        static::_print('WARNING', $message, $params);
         return 2;
     }
-
+    
     public static function success($message, ...$params)
     {
-        static::log('SUCCESS', $message, $params);
+        static::_print('SUCCESS', $message, $params);
         return 0;
     }
-
-    protected static function log($type, $message, $params)
+    
+    protected static function _print($type, $message, $params): void
     {
-        if (static::$daemonize) {
+        if (static::$disabledOutput) {
             return;
         }
         if (strpos($message, '{0}') !== FALSE) {
             foreach ($params as $k => $v) {
-                $v = is_string($v) ? $v : json_encode($v);
+                $v       = is_string($v) ? $v : json_encode($v);
                 $message = str_replace('{' . $k . '}', "[{$v}]", $message);
             }
         } else {
             $explode = explode('{}', $message);
-            $msg = '';
+            $msg     = '';
             foreach ($explode as $k => $value) {
                 $msg .= $value;
                 if (isset($params[$k])) {
@@ -65,8 +64,10 @@ class ConsoleOutput
             }
             $message = $msg;
         }
-
-
-        printf("[%s][%s]: %s\n", date('Y-m-d H:i:s'), $type, $message);
+        $s = sprintf("[%s][%s]: %s\n", date('Y-m-d H:i:s'), $type, $message);
+        echo $s;
+        if (defined(STDERR) && ($type == 'ERROR' || $type == 'WARNING')) {
+            fwrite(STDERR, $s);
+        }
     }
 }
