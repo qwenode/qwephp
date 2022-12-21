9eb1391c4a4befc09afc8b37e7277717cff359cd
Node <8974108+qwenode@users.noreply.github.com>
2022-12-21 18:05:23 +0800

    remove emoji

--- src/SS.php
+++ src/SS.php
@@ -485,6 +485,11 @@ class SS
         return $filename;
     }
     
+    public static function removeEmoji($str)
+    {
+        return preg_replace('/[\x{1F600}-\x{1F64F}]|[\x{1F300}-\x{1F5FF}]|[\x{1F680}-\x{1F6FF}]|[\x{2600}-\x{26FF}]|[\x{2700}-\x{27BF}]/u', '', $str);
+    }
+    
     /**
      * remove special char
      * @param string $str
--- tests/SanitizeTest.php
+++ tests/SanitizeTest.php
@@ -34,6 +34,7 @@ class SanitizeTest extends Unit
         $this->assertEquals(SS::toFloat(' abc1.223#.3 d'), '1.223');
         $this->assertEquals(SS::extractFirstInt(' abc1.223#.3 d'), '1');
         $this->assertEquals(SS::extractFirstInt(' abc12.2223#.3 d'), '12');
+        $this->assertEquals(SS::removeEmoji('外贸 Dunk 11色福利上架✔️ VT纯原 “618”携手工厂 现货优势 原3XX 品质 相册实拍 售后包到底 放心冲刺🚀🚀'), '外贸 Dunk 11色福利上架️ VT纯原 “618”携手工厂 现货优势 原3XX 品质 相册实拍 售后包到底 放心冲刺');
     }
 
 }
