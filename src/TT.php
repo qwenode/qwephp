d2e1b417a29f2c3be3b5563eeb62cb1bea3dc3ce
Node <8974108+qwenode@users.noreply.github.com>
2023-01-12 15:38:52 +0800

    format day

--- src/TT.php
+++ src/TT.php
@@ -9,6 +9,8 @@ class TT
     /**
      * @param int $unixTime
      * @return int
+     * @deprecated
+     * @see formatToDayBegin
      */
     public static function begin(int $unixTime)
     {
@@ -18,11 +20,51 @@ class TT
     
     /**
      * @param int $unixTime
+     * @return false|int
+     */
+    public static function formatToDayBegin(int $unixTime)
+    {
+        $x = date('Y-m-d 00:00:00', $unixTime);
+        return strtotime($x);
+    }
+    /**
+     * @param int $unixTime
      * @return int
+     * @deprecated
+     * @see formatToDayEnd
      */
     public static function end(int $unixTime)
     {
         $x = date('Y-m-d 23:59:59', $unixTime);
         return strtotime($x);
+    }
+    
+    /**
+     * @param int $unixTime
+     * @return false|int
+     */
+    public static function formatToDayEnd(int $unixTime)
+    {
+        $x = date('Y-m-d 23:59:59', $unixTime);
+        return strtotime($x);
+    }
+    public static function formatToMonthBegin(int $unixTime)
+    {
+       return strtotime(date('Y-m 00:00:00',$unixTime));
+    }
+    
+    public static function formatToMonthEnd(int $unixTime)
+    {
+        return strtotime(date('Y-m-t 23:59:59',$unixTime));
+    }
+    public static function getCurrentMonthBegin()
+    {
+        $strtotime = strtotime(date('Y-m 00:00:00'));
+        return $strtotime;
+    }
+    
+    public static function getCurrentMonthEnd()
+    {
+        return strtotime(date('Y-m-t 23:59:59'));
     }
 }
--- tests/DateTimesTest.php
+++ tests/DateTimesTest.php
@@ -18,7 +18,12 @@ class DateTimesTest extends Unit
     public function testSomeFeature()
     {
         date_default_timezone_set('Asia/Shanghai');
-        $this->assertEquals(TT::begin(1600165627), 1600099200);
-        $this->assertEquals(TT::end(1600165633), 1600185599);
+        $this->assertEquals(TT::formatToDayBegin(1600165627), 1600099200);
+        $this->assertEquals(TT::formatToDayEnd(1600165633), 1600185599);
+//        $this->assertEquals(TT::getCurrentMonthBegin(), 1672502400);
+//        $this->assertEquals(TT::getCurrentMonthEnd(), 1675180799);
+        $this->assertEquals(TT::formatToMonthBegin(1675180799), 1672502400);
+        $this->assertEquals(TT::formatToMonthEnd(1675180791), 1675180799);
     }
+    
 }
